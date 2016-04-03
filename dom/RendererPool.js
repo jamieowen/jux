b611d9516c828816a93c1d741aed2425e36c88e8
jamieowen <hello@jamieowen.com>
2016-04-03 18:28:28 +0100

    Pool tabbing fix

--- dom/RendererPool.js
+++ dom/RendererPool.js
@@ -5,11 +5,10 @@ var DomRendererPool = RendererPool.extend( {
 
 	create: function( data ){
 
-		console.log( 'CREATE' );
 		var ele = document.createElement( 'div' );
-		ele.style.position 			= 'absolute';
-		ele.style.display  			= 'block';
-		ele.style.backgroundColor  	= '#999999';
+		ele.style.position = 'absolute';
+		ele.style.display = 'block';
+		ele.style.backgroundColor = '#999999';
 
 		return ele;
 
